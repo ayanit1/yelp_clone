1ee5749fec0833de47e08e8f29cdc2676d01f7dd
Alice Armstrong <tioma001@hotmail.com>
2017-04-04 12:17:28 +0100

    Migrating database

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Review, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe Review do
+  let(:review){described_class.new}
+
+  it "tests database" do
+  expect{review.create(rating: 5, comment: "Very good")}.to change(Review, :count)
+  end
+  
 end
