7ba3c2e9a7e3ad3b8f0eb15e0c266866a9e160f5
Ryan <Ryanchu.ws@gmail.com>
2017-04-04 15:55:59 +0100

    review creation now supported

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,23 @@
 class ReviewsController < ApplicationController
-  
+
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to '/reviews'
   end
 
   def index
     @reviews = Review.all
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -12,4 +12,15 @@ feature "displays views" do
     visit '/reviews'
     expect(page).to have_link('Add a review')
   end
+
+feature "add a review" do
+  scenario "user adds a review using a form" do
+    visit '/reviews'
+    click_link 'Add a review'
+    fill_in 'review_rating', with: '5'
+    fill_in 'review_comment', with: 'very tasty'
+    click_button 'Submit'
+    expect(page).to have_content('very tasty')
+  end
+end
 end
