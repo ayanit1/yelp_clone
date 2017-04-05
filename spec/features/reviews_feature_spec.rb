639ff2ed79418ae1bb7a4b4107c4da27867ab70c
Flora Harvey <fharvey555@gmail.com>
2017-04-05 16:10:48 +0100

    Refactor avg rating method

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -8,17 +8,16 @@ feature "displays reviews" do
     click_link 'Tayyabs'
     expect(page).to have_link('Add a review')
   end
-end
 
-    scenario "user adds a review using a form" do
-      Restaurant.create(name: 'Tayyabs', description: 'Has food.')
-      visit '/restaurants'
-      click_link 'Tayyabs'
-      click_link 'Add a review'
-      fill_in 'review_rating', with: '5'
-      fill_in 'review_comment', with: 'very tasty'
-      click_button 'Submit'
-      expect(current_path).to eq('/restaurants/1')
-      expect(page).to have_content('very tasty' && '5')
-    end
+  scenario "user adds a review using a form" do
+    Restaurant.create(name: 'Tayyabs', description: 'Has food.')
+    visit '/restaurants'
+    click_link 'Tayyabs'
+    click_link 'Add a review'
+    fill_in 'review_rating', with: '5'
+    fill_in 'review_comment', with: 'very tasty'
+    click_button 'Submit'
+    expect(current_path).to eq('/restaurants/1')
+    expect(page).to have_content('very tasty' && '5')
+  end
 end
